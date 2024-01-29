48e0bb49572193b8609ea2da185bba2b23a50ec4
it-rsu-pindad <sautomated01@gmail.com>
2024-01-29 16:24:15 +0700

    add nullable to pic_id

--- database/migrations/2024_01_26_015548_create_user_profile_table.php
+++ database/migrations/2024_01_26_015548_create_user_profile_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             // $table->foreignId('user_id')->contrained()->onDelete('cascade');
             $table->foreignUuid('user_id');
-            $table->bigInteger('pic_id')->unsigned()->index();
+            $table->bigInteger('pic_id')->unsigned()->index()->nullable();
             // $table->foreignUuid('pic_id');
             $table->string('nama_profile')->nullable();
             $table->string('nomor_handphone_profile')->unique()->nullable();
