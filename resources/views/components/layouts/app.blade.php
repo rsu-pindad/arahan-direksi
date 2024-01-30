3be4679a025d21f66d23ea72dd15aa83e8773ea3
it-rsu-pindad <sautomated01@gmail.com>
2024-01-30 13:44:40 +0700

    add admin lte template

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,49 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="stylesheet" href="{{ asset("vendor/adminlte/dist/css/adminlte.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("vendor/fontawesome-free/css/all.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("vendor/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
+
+        <script src="{{ asset("vendor/jquery/jquery.min.js") }}"></script>
+        <script src="{{ asset("vendor/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+        <script src="{{ asset("vendor/adminlte/dist/js/adminlte.min.js") }}"></script>
+        <!-- <script src="{{asset("vendor\popper\popper.min.js")}}"></script> -->
+
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
+    @if(Route::is('login') or Route::is('register'))
+    <body class="login-page">
+    @elseif(Route::is('beranda'))
+    <body class="sidebar-mini">
+    @else
     <body>
-        {{ $slot }}
+    @endif
+    
+        <!-- Wrapper -->
+        <div class="wrapper">
+            @auth
+            <!-- Navbar  -->
+            @include('components.layouts.navbar')
+            <!-- End Navbar  -->
+
+            <!-- Sidebar -->
+            @include('components.layouts.sidebar')
+            <!-- End Sidebar -->
+
+            <!-- Content -->
+            {{ $slot }}
+            <!-- End Content -->
+
+            <!-- Footer -->         
+            @include('components.layouts.footer')
+            <!-- End Footer -->
+
+            @endauth
+            @livewireScripts
+
+        </div>
+        <!-- End Wrapper -->
+
     </body>
 </html>
