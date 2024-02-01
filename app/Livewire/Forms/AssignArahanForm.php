f36d348f6f68e0ee10b852f7aa87206b5cab19b2
it-rsu-pindad <sautomated01@gmail.com>
2024-02-01 16:55:52 +0700

    add modul assign arahan

---
+++ app/Livewire/Forms/AssignArahanForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+use Illuminate\Validation\Rule;
+use App\Models\PivotArahanProgress As Pivot;
+
+
+class AssignArahanForm extends Form
+{
+    public ?Pivot $pivot;
+
+    public $arahan_id = '';
+    public $progress_id = '';
+
+    public function rules()
+    {
+        return [
+            'arahan_id' => [
+                'required',
+            ],
+            'progress_id' => [
+                'required',
+            ]
+        ];
+    }
+
+    public function setPost(Pivot $pivot)
+    {
+        // $this->user_id = auth()->user()->id;
+        $this->arahan_id = $pivot->arahan_id;
+        $this->progress_id = $pivot->progress_id;
+    }
+
+    function store()
+    {
+        try {
+            $this->validate();
+            // dd($this->validate());
+            Pivot::updateOrCreate(
+                $this->only([
+                    'arahan_id',
+                    'progress_id'
+                ])
+            );
+            $this->reset();
+            session()->flash('success', 'assign berhasil');
+        } catch (\Illuminate\Database\QueryException $exception) {
+            session()->flash('failure',$exception->getMessage());
+        }
+    }
+
+}
