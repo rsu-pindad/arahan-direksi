0a662e540acb677f090bd7737bf4aff29925099b
it-rsu-pindad <sautomated01@gmail.com>
2024-02-01 16:56:16 +0700

    still bugs

--- app/Livewire/Progress/ProgressArahan.php
+++ app/Livewire/Progress/ProgressArahan.php
@@ -4,13 +4,34 @@ namespace App\Livewire\Progress;
 
 use Livewire\Component;
 use App\Models\MasterProgress;
+use App\Models\UserProfile;
+use App\Models\PivotArahanProgress;
 
 class ProgressArahan extends Component
 {
+    #[Reactive]
+    public $pivot_id = '';
+    public $progress_id = '';
+
     public function render()
     {
         return view('livewire.progress.progress-arahan')->with([
             'progress' => MasterProgress::orderBy('id')->get(),
         ]);
+    }
+
+    public function update()
+    {
+        try {
+            $pivArahan = PivotArahanProgress::find('id', $pivot_id);
+            $pivArahan->update(
+                $this->only([
+                    'progress_id',
+                ])
+            );
+            // $this->dispatch('$refresh');
+        } catch (\Illuminate\Database\QueryException $exception) {
+            session()->flash('failure', $exception->getMessage());
+        }
     }
 }
