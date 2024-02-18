92aaea408a7bbd3f0b86314a3f47195f85bc26ac
it-rsu-pindad <sautomated01@gmail.com>
2024-02-18 18:20:32 +0700

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\BerandaController;
 use App\Livewire\Beranda;
 use App\Livewire\LoginUser;
 use App\Livewire\RegisterUser;
@@ -38,41 +36,47 @@ use App\Livewire\Arahan\StepperArahan;
 |
 */
 
-// Route::get('/', [BerandaController::class, 'render'])->name('beranda');
+Auth::routes();
 
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', function(){
     return Redirect::intended('/login');
 });
-Route::get('/beranda', Beranda::class)->name('beranda');
 
-// user auth
-Route::get('/login', LoginUser::class)->name('login');
-Route::get('/logout', [LoginUser::class,'logout'])->name('logout');
-Route::get('/register', RegisterUser::class)->name('register');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', RegisterUser::class)->name('register');
+    Route::get('/login', LoginUser::class)->name('login');
+});
 
-// PIC 
-Route::get('/pic', Pic::class)->name('pic');
-Route::get('/pic/table', TablePic::class)->name('pic-table');
-Route::get('/pic/view/{id}', PicView::class)->name('pic-view');
-Route::get('/pic/edit/{id}', PicEdit::class)->name('pic-edit');
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/logout', [LoginUser::class,'logout'])->name('logout');
+    Route::get('/beranda', Beranda::class)->name('beranda');
+    
+    // Pic
+    Route::get('/pic', Pic::class)->name('pic');
+    Route::get('/pic/table', TablePic::class)->name('pic-table');
+    Route::get('/pic/view/{id}', PicView::class)->name('pic-view');
+    Route::get('/pic/edit/{id}', PicEdit::class)->name('pic-edit');
 
-// User Profile
-Route::get('/profile', UserProfile::class)->name('profile');
-Route::get('/profile/table', TableProfile::class)->name('profile-table');
+    // User Profile
+    Route::get('/profile', UserProfile::class)->name('profile');
+    Route::get('/profile/table', TableProfile::class)->name('profile-table');
+    
+    // Master Progress
+    Route::get('/progress', Progress::class)->name('progress');
+    Route::get('/progress/view/{id}', ProgressView::class)->name('progress-view');
+    Route::get('/progress/edit/{id}', ProgressEdit::class)->name('progress-edit');
+    Route::get('/progress/table', TableProgress::class)->name('progress-table');
 
-// Master Progress
-Route::get('/progress', Progress::class)->name('progress');
-Route::get('/progress/view/{id}', ProgressView::class)->name('progress-view');
-Route::get('/progress/edit/{id}', ProgressEdit::class)->name('progress-edit');
-Route::get('/progress/table', TableProgress::class)->name('progress-table');
+    // Master Arahan
+    Route::get('/arahan', StepperArahan::class)->name('arahan');
 
-// Master Arahan
-// Route::get('/arahan', Arahan::class)->name('arahan');
-Route::get('/arahan', StepperArahan::class)->name('arahan');
+    // Assign Arahan / kanban arahan
+    Route::get('/assign-arahan', AssignArahan::class)->name('assign-arahan');
 
-// Assign Arahan / kanban arahan
-Route::get('/assign-arahan', AssignArahan::class)->name('assign-arahan');
+    // Progress Arahan
+    Route::get('/progress-arahan', ProgressArahan::class)->name('progress-arahan');
+    Route::get('/progress-arahan/{id}', ProgressArahanOpen::class)->name('progress-arahan-detail');
 
-// Progress Arahan
-Route::get('/progress-arahan', ProgressArahan::class)->name('progress-arahan');
-Route::get('/progress-arahan/{id}', ProgressArahanOpen::class)->name('progress-arahan-detail');
+});
